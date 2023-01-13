2d0d0e7d217c4a5bc7fe7ecc464fb45f835f3179
kalemat00 <kalemat@hotmail.it>
2023-01-13 12:46:48 +0100

    second commit

--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,14 +1,13 @@
 fun main() {
-    val list1 = mutableListOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    val list2 = mutableListOf<String>()
-    list1.forEach { list2.add(it.toString()) }
+    val list: List<Int> = listOf(1, 2, 3, 344, 454, 5, 6, 8, 433, 99)
+    list.filterList().printListOfInt()
+}
 
-    println("in the list1 the member of the list are integer(the syntax it + it is used as sum \n" +
-            "and not as concatenation of strings):")
-    list1.forEach { println(it + it) }
+fun List<Int>.filterList(): List<Int>{
+    return this.filter { it < 100 }
+}
 
-    println("in the list2 the member of the list are Strings(they aren't added as integer):")
-    list2.forEach { println(it + it) }
-
+fun List<Int>.printListOfInt(){
+    this.forEach { print("$it ") }
 }
 
